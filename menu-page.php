db517894c478d3e0fe58153e663588c540f38d50
lescha19891 <lescha19891@tut.by>
2020-11-24 01:01:13 +0300

    Commit4

---
+++ menu-page.php
@@ -0,0 +1,4 @@
+    <h1 class="wp-heading">КУРСЫ ВАЛЮТ</h1>
+    <?PHP $money=GET_OPTION("kurs" );
+    var_dump($money);
+    ?>
--- money.php
+++ money.php
@@ -1,57 +1,38 @@
 <?php
-/*
-	Plugin Name: money
-    */
-add_action('after_setup_theme','rubToMoney');
- function rubToMoney(){
-	 //получение данных от Нацбанка
-	 //соединение с БД
-	 	$servername = "localhost";
-		  $username = DB_USER; 
-		  
-	 	$password = DB_PASSWORD;
-	 	$dbname = DB_NAME;
-	 	$conn = mysqli_connect($servername, $username, $password, $dbname);
-			if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-	 //проверка на существование и создание таблицы
-		$sql = "CREATE TABLE IF NOT EXISTS  money_to_rub (
-		ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-       	USD FLOAT UNSIGNED,
-        EUR FLOAT UNSIGNED,
-        RUR FLOAT UNSIGNED,
-        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
-		mysqli_query($conn, $sql);
-	 
-	 //Запись данных Нацбанка в таблицу ,???ЗАПИСЫВАЕТ В БАЗУ 2 ЗН6АЧЕНИЯ???
-	 $USD=2.55; $EUR=3.4; $RUR=0.5;
-	 $sql="INSERT INTO money_to_rub ( USD, EUR, RUR)  VALUES ($USD, $EUR, $RUR)";
-		mysqli_query($conn, $sql);
-
-	 //вывод последней записи в массив
-
-        $sql = "SELECT * FROM money_to_rub order by ID desc	limit 1";
-		$result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array ($result); 
-		$money_rub[] =[
-        "ID"=>$row ['ID'],
-        "USD"=>$row['USD'],
-        "EUR"=>$row['EUR'],
-		"RUR"=>$row [ 'RUR']
+	/*	Plugin Name: money	*/
+	register_activation_hook(__FILE__,'activationmoney');
+ 	function activationmoney(){
+	//переустанавливаем крон задачи относящиеся к плагину
+		wp_clear_scheduled_hook('update_curse');
+		wp_schedule_event( time(), 'twicedaily', 'update_curse');
+		updaterubusdeur();
+	}
+	add_action('update_curse', 'updaterubusdeur');
+	function updaterubusdeur(){
+	//получение данных с сайта НБРБ и запись в wp
+		$url=['USD'=>'https://www.nbrb.by/api/exrates/rates/usd?parammode=2',
+		 'EUR'=>'https://www.nbrb.by/api/exrates/rates/eur?parammode=2',
+		 'RUB'=>'https://www.nbrb.by/api/exrates/rates/rub?parammode=2'
 		];
-	 //Очистка базы если записей больше 10
-		if ($row ['ID']>10){
-			$sql= "DROP TABLE  money_to_rub ";
-			mysqli_query($conn, $sql);
-		}	
-		mysqli_close($conn);
-		return  $money_rub; 
-	}	
-	 //Создание меню
+		$usd=wp_remote_get( $url['USD'] );
+		$usd=wp_remote_retrieve_body($usd);
+		$usd=json_decode($usd, true);
+		$eur=wp_remote_get( $url['EUR'] );
+		$eur=wp_remote_retrieve_body($eur);
+		$eur=json_decode($eur, true);
+		$rub=wp_remote_get( $url['RUB'] );
+		$rub=wp_remote_retrieve_body($rub);
+		$rub=json_decode($rub, true);
+		$RUB=$rub["Cur_OfficialRate"];
+		$USD=$usd["Cur_OfficialRate"];
+		$EUR=$eur["Cur_OfficialRate"];
+		$money=["RUB"=>$RUB,"USD"=>"$USD","EUR=>$EUR"];
+		update_option('kurs',$money);
+	}
+	//Создание меню
 	add_action( 'admin_menu', function(){
-
-        add_menu_page(
+		$money=get_option('kurs');
+	        add_menu_page(
             'rub_to_money',
             'roublerate',
             'manage_options',
@@ -60,5 +41,5 @@ add_action('after_setup_theme','rubToMoney');
             plugins_url( 'money/images/menu-icon.svg' ),
             83
 		);
-	})
+	});
 ?>
