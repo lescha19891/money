72fd44b8441d161f6d2ba8d728f8aa20a6538b58
Alexandr <zankoav@mail.ru>
2020-12-03 19:46:18 +0300

    Refactoring

--- money.php
+++ money.php
@@ -1,27 +1,35 @@
 <?php
 	/*	Plugin Name: money	*/
 	register_activation_hook(__FILE__,'activationmoney');
+
  	function activationmoney(){
 	//переустанавливаем крон задачи относящиеся к плагину
 		wp_clear_scheduled_hook('update_curse');
 		wp_schedule_event( time(), 'twicedaily', 'update_curse');
 		updaterubusdeur();
 	}
+
 	add_action('update_curse', 'updaterubusdeur');
+
 	function updaterubusdeur(){
 		//получение данных с сайта НБРБ и запись в wp
-		$url='https://www.nbrb.by/api/exrates/rates?periodicity=0';
-		$money=wp_remote_get($url);
-		$money=wp_remote_retrieve_body($money);
-		$mon=json_decode($money, true);
-		foreach($mon as $v){
-			global $money;
-			$money[]=[$v['Cur_Abbreviation']=>$v['Cur_OfficialRate']];
+		$url = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
+		$response = wp_remote_get($url);
+		$responseBody = wp_remote_retrieve_body($response);
+		$currencies = json_decode($responseBody, true);
+		$result = [
+			'date' => $currencies[0]['Date'];
+			'currencies' => []	
+		];
+		foreach($currencies as $currency){
+			$result['currencies][] = [
+				$currency['Cur_Abbreviation'] => $currency['Cur_OfficialRate']
+			];
 		}
-		$money=call_user_func_array('array_merge',$money);
-		$money['date']=date(("Y.m.d"));
-		update_option('kurs',$money);
+		
+		update_option('kurs', $result);
 	}
+	
 	//Создание меню
 	add_action( 'admin_menu', function(){
 	        add_menu_page(
@@ -34,9 +42,11 @@
             83
 		);
 	});
+	
 	//удаление плагина
 	register_deactivation_hook( __FILE__, 'money_del' );
+	
 	function money_del() {
 		wp_clear_scheduled_hook( 'update_curse' );
-			}
+	}
 ?>
