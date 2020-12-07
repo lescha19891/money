f71ac26d8b7c6d3a61684a5f67bb2626e505e277
lescha19891 <lescha19891@tut.by>
2020-12-07 18:29:45 +0300

    refactoring code

--- money.php
+++ money.php
@@ -1,23 +1,22 @@
 <?php
 	/*	Plugin Name: Currency BYN	
-	Plugin URI: https://wordpress.org/plugins/Currency BYN
-	Description: Shows currency exchange rates for Belarus
-	Version: 0.1.0
-	Author: LS
-	Author URI: 
-	Text Domain:
+		Description: Shows currency exchange rates for Belarus
+		Version: 1.0.0
+		Author: Lightning-Soft
+		Text Domain: ls_cb
 	*/
-	register_activation_hook(__FILE__,'activationmoney');
 
- 	function activationmoney(){
-		wp_clear_scheduled_hook('update_curse');
-		wp_schedule_event( time(), 'twicedaily', 'update_curse');
-		updaterubusdeur();
+	register_activation_hook(__FILE__,'ls_cb_activationmoney');
+
+ 	function ls_cb_activationmoney(){
+		wp_clear_scheduled_hook('ls_cb_update_curse');
+		wp_schedule_event( time(), 'twicedaily', 'ls_cb_update_curse');
+		ls_cb_updaterubusdeur();
 	}
 
-	add_action('update_curse', 'updaterubusdeur');
+	add_action('ls_cb_update_curse', 'ls_cb_updaterubusdeur');
 
-	function updaterubusdeur(){
+	function ls_cb_updaterubusdeur(){
 		$url = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
 		$response = wp_remote_get($url);
 		$responseBody = wp_remote_retrieve_body($response);
@@ -36,15 +35,34 @@
 		
 		update_option('kurs', $result);
 	} 
-	function GET_currency($rate){
+
+	function ls_cb_get_currency_by_abbreviation($rate){
+		$res=null;
 		$result = get_option('kurs');
 		foreach ($result['currencies'] as $k=>$v){
 			if ($result['currencies']["$k"]['Cur_Abbreviation']==$rate){
 				$res=$result['currencies']["$k"]['Cur_OfficialRate'];
+				break;
 			}
 		}
 		return $res;
-		}
+	}
+
+	function ls_cb_get_currencies(){
+		return get_option('kurs')['currencies'];
+	}
+
+	function ls_cb_get_usd_rate(){
+		return ls_cb_get_currency_by_abbreviation('USD');
+	}
+
+	function ls_cb_get_eur_rate(){
+		return ls_cb_get_currency_by_abbreviation('EUR');
+	}
+
+	function ls_cb_get_rub_rate(){
+		return ls_cb_get_currency_by_abbreviation('RUB');
+	}
 
 	add_action( 'admin_menu', function(){
 	        add_menu_page(
@@ -58,9 +76,9 @@
 		);
 	});
 	
-	register_deactivation_hook( __FILE__, 'money_del' );
+	register_deactivation_hook( __FILE__, 'ls_cb_money_del' );
 	
-	function money_del() {
-		wp_clear_scheduled_hook( 'update_curse' );
+	function ls_cb_money_del() {
+		wp_clear_scheduled_hook( 'ls_cb_update_curse' );
 	}
 ?>
